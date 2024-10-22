6ed09ca72b1bbc1b8cf9b57aa42d3c2c03748ab1
FOXMIX BRICO <cauamoreira2k@gmail.com>
2024-10-22 09:37:40 -0300

    atualização

--- sist2.py
+++ sist2.py
@@ -5,6 +5,7 @@ import streamlit as st
 from datetime import datetime,timedelta
 import os
 from babel.numbers import format_currency
+import matplotlib.pyplot as plt
 
 # Configuração da página com título e favicon
 st.set_page_config(
@@ -201,27 +202,19 @@ def create_percentage_chart(df):
 
 # Função para criar o gráfico de barras com o valor total em R$ apenas para status Pendente e Atrasado
 def create_value_bar_chart(df):
-    # Converte a coluna 'Valor Total' para numérico
-    df['Valor Total Numérico'] = df['Valor Total'].apply(lambda x: locale.atof(x.strip()))
+    # Remove o símbolo de moeda e os separadores de milhar, depois converte para numérico
+    df['Valor Total Numérico'] = df['Valor Total'].replace({'R\$': '', ',': ''}, regex=True).astype(float)
 
-    # Filtra o DataFrame para incluir os status "Pendente", "Atrasado" e "Entregue"
+    # Filtra o DataFrame para incluir os status "Pendente", "Atrasado", "Entregue"
     df_filtrado = df[df['Status'].isin(['Pendente', 'Atrasado', 'Entregue'])]
 
-    # Agrupa os dados por status e calcula o valor total em R$
-    total_por_status = df_filtrado.groupby('Status')['Valor Total Numérico'].sum().reset_index()
-    total_por_status.columns = ['Status', 'Valor Total']
+    # Cria o gráfico de barras com o valor total
+    df_filtrado.groupby('Status')['Valor Total Numérico'].sum().plot(kind='bar', color=['red', 'yellow', 'green'])
 
-    # Cria o gráfico de barras
-    bar_chart = px.bar(
-        total_por_status, 
-        x='Status', 
-        y='Valor Total', 
-        text='Valor Total', 
-        title='Valor Total por Status',
-        labels={'Valor Total': 'Valor Total (R$)', 'Status': '  '}
-    )
-    
-    return bar_chart
+    # Exibe o gráfico
+    plt.title('Total por Status (R$)')
+    plt.ylabel('Valor Total (R$)')
+    plt.show()
 
 def guia_dashboard():
     # Cabeçalho para Estatísticas Gerais
