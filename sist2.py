fdb79c01579d5a81ac67857508f9bb5c4338fdf7
FOXMIX BRICO <cauamoreira2k@gmail.com>
2024-10-25 11:08:42 -0300

    att

--- sist2.py
+++ sist2.py
@@ -262,8 +262,8 @@ def create_percentage_chart(df):
 
 # Função para criar o gráfico de barras com o valor total em R$ apenas para status Pendente e Atrasado
 def create_value_bar_chart(df):
-    # Converte a coluna 'Valor Total' para numérico
-    df['Valor Total Numérico'] = df['Valor Total'].replace({'\.': '', ',': '.'}, regex=True).astype(float)
+    # Removendo 'R$' e formatando para numérico
+    df['Valor Total Numérico'] = df['Valor Total'].replace({'R\$': '', '\.': '', ',': '.'}, regex=True).astype(float)
 
     # Filtra o DataFrame para incluir os status "Pendente", "Atrasado" e "Entregue"
     df_filtrado = df[df['Status'].isin(['Pendente', 'Atrasado', 'Entregue'])]
