22f624b3a3f22a5a5b808b61e989250cfb0c11c8
FOXMIX BRICO <cauamoreira2k@gmail.com>
2024-12-18 12:37:52 -0300

    att

--- sist.py
+++ sist.py
@@ -275,7 +275,7 @@ def guia_dashboard():
     col1, col2, col3, col4= st.columns([4,1,1,3])
     
     with col1:
-        st.markdown("<h3>📊 Estatísticas Gerais <small style='font-size: 0.4em;'>atualizado dia 17/12 às 11:00</small></h3>", unsafe_allow_html=True)
+        st.markdown("<h3>📊 Estatísticas Gerais <small style='font-size: 0.4em;'>atualizado dia 18/12 às 11:00</small></h3>", unsafe_allow_html=True)
     with col4:
         valor_total_entregues = df_carteira[df_carteira['Status'] == 'Entregue']['Valor Total'].sum()
         st.metric("Faturamento Total", 
@@ -325,7 +325,7 @@ def guia_dashboard():
 
     st.markdown("<h3>🏢 Setores</h3>", unsafe_allow_html=True)
 
-    col1, col2, col3, col4, col5, col6, col7, col8 = st.columns(8)
+    col1, col2, col3, col4 = st.columns(4)
 
     pendencia_separacao = len(separacao[separacao['Status'] == 'Pendente'])
     pendencia_compras = len(compras[compras['Status'] == 'Pendente'])
@@ -342,44 +342,21 @@ def guia_dashboard():
     total_embalagem = len(embalagem.index)
     total_expedicao = len(expedicao.index)
 
-    st.markdown("""
-            <style>
-            .stButton>button {
-                background-color: transparent;
-                border: none;
-                font-size: 24px;
-                cursor: pointer;
-            }
-            </style>
-        """, unsafe_allow_html=True)
-
     with col1:
         st.metric("Separação", total_separacao)
         st.markdown(f"<span style='font-size: 0.8em; margin-top: -10px; display:inline-block;'>P {pendencia_separacao} | A {atraso_separacao}</span>", unsafe_allow_html=True)
 
     with col2:
-        st.button("👁️", key="separacao_button", on_click=lambda: guia_separacao())
-
-    with col3:
         st.metric("Compras", total_compras)
         st.markdown(f"<span style='font-size: 0.8em;'>P {pendencia_compras} | A {atraso_compras}</span>", unsafe_allow_html=True)
 
-    with col4:
-        st.button("👁️", key="compras_button", on_click=lambda: guia_compras())
-
-    with col5:
+    with col3:
         st.metric("Embalagem", total_embalagem)
         st.markdown(f"<span style='font-size: 0.8em;'>P {pendencia_embalagem} | A {atraso_embalagem}</span>", unsafe_allow_html=True)
 
-    with col6:
-        st.button("👁️", key="embalagem_button", on_click=lambda: guia_embalagem())
-
-    with col7:
+    with col4:
         st.metric("Expedição", total_expedicao)
         st.markdown(f"<span style='font-size: 0.8em;'>P {pendencia_expedicao} | A {atraso_expedicao}</span>", unsafe_allow_html=True)
-
-    with col8:
-        st.button("👁️", key="expedicao_button", on_click=lambda: guia_expedicao())
 
     col_graph1, col_graph2 = st.columns(2)
     
@@ -612,4 +589,3 @@ def guia_OE():
 
 if perfil_opcao == "Não gerado OE ❌":
     guia_OE()
-
